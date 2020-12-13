d2fe3daef74604c33e1cfd070d8b19c822f10275
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-12-13 00:58:33 +0000

    remove use of

--- angularjs-1.5+/customer/customer.controller.js
+++ angularjs-1.5+/customer/customer.controller.js
@@ -2,16 +2,18 @@ angular
     .module('app.customer', [])
     .component('customerComponent', {
         templateUrl: 'customer/customer.html',
-        controller: CustomerController
+        controller: CustomerController,
+        controllerAs: 'ctrl'
     });
 
-function CustomerController($scope, customerService) {
+function CustomerController(customerService) {
     const self = this;
     self.users = users;
+    self.$onInit = () => {
+        self.customers = users();
+    }
 
     function users() {
         return customerService.users();
     }
-
-    $scope.customers = users();
 }
