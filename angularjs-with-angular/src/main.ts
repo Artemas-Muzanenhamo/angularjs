97e7d25aabab4e3c3467733f7451fa024f2d818c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-01-30 23:38:38 +0000

    amend main.ts

--- angularjs-with-angular/src/main.ts
+++ angularjs-with-angular/src/main.ts
@@ -1,4 +1,4 @@
-// import 'core-js/fn/reflect';
+import 'core-js/features/reflect';
 // import 'core-js/library/shim';
 import 'zone.js/dist/zone';
 
@@ -8,6 +8,6 @@ import {AppModule} from "./app/app.module";
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
     const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
-    upgrade.bootstrap(document.documentElement, ['app']);
+    upgrade.bootstrap(document.documentElement, ['app2']);
     console.log('Hybrid App Bootstraped!');
 });
